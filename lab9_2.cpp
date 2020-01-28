0b08157993544077422e37e5bf1d833f67c14169
ZenPtc <maku4fb@gmail.com>
2020-01-28 16:21:44 +0700

    lab9.2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,3 +2,22 @@
 //cout << textline;
 //cout << "-------------------- SOTUS ---------------------";
 //Don't forget to change cout to output filestream
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+
+    ifstream source("C:\\Users\\wisaTong\\Desktop\\ComPro\\Lab\\lab9-2562-2-ZenPtc\\cheerbook.txt");
+    ofstream dest("C:\\Users\\wisaTong\\Desktop\\ComPro\\Lab\\lab9-2562-2-ZenPtc\\cheerbook_copy.txt");
+    string textline;
+    dest << "-------------------- SOTUS ---------------------" << endl;
+    while(getline(source,textline)){
+        dest << textline << endl;
+    }
+    dest << "-------------------- SOTUS ---------------------" << endl;
+    source.close();
+    dest.close();
+
+    return 0;
+}
